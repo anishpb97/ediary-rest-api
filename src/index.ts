c18353530aca6bf5153a57eba1362ae0b4775cb2
Anish P B <anishbalasubramanie@gmail.com>
2022-01-15 16:20:00 +0530

    chore(actuator): added actuator endpoint for checking app state

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
+/* eslint-disable no-undef */
 import dotenv from 'dotenv';
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 
 dotenv.config();
 
 const app: Application = express();
 
+app.get('/health', (request:Request, response:Response) => {
+  response.send('App is up and running...');
+});
+
 const { PORT } = process.env;
+// eslint-disable-next-line no-console
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}!`));
